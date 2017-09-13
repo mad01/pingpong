a6d92ccaacbb80661d63509145f4017c14967ce7
Alexander Brandstedt <alexanderbrandstedt@gmail.com>
2017-09-13 13:56:42 +0200

    implemnet init serv of grpc and prometheus in main

--- main.go
+++ main.go
@@ -3,16 +3,36 @@ package main
 import (
 	"flag"
 	"fmt"
+	"log"
+	"net"
+	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+
+	"github.com/grpc-ecosystem/go-grpc-middleware"
+	"github.com/grpc-ecosystem/go-grpc-prometheus"
+	"github.com/prometheus/client_golang/prometheus/promhttp"
+
+	pb "github.com/mad01/pingpong/com"
+	"golang.org/x/net/context"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/reflection"
 )
 
+//
+// Server
+//
 type config struct {
-	ServerPort int
-	Version    bool
+	grpcAddr string
+	httpAddr string
+	Version  bool
 }
 
-func newCmd() *config {
+func newServerCmd() *config {
 	c := new(config)
-	flag.IntVar(&c.ServerPort, "server.port", 8888, "server port")
+	flag.StringVar(&c.grpcAddr, "grpc.addr", "0.0.0.0:8881", "grpc server port")
+	flag.StringVar(&c.httpAddr, "http.addr", "0.0.0.0:8882", "http server port")
 	flag.BoolVar(&c.Version, "version", false, "show version")
 	flag.Parse()
 
@@ -23,6 +43,81 @@ func newCmd() *config {
 	return c
 }
 
+type server struct{}
+
+func (s *server) Ping(ctx context.Context, in *pb.PingRequest) (*pb.PongResponse, error) {
+	response := pb.PongResponse{Msg: in.Msg}
+	return &response, nil
+}
+
+func serveGRPC(addr string, errChan chan error) {
+	lis, err := net.Listen("tcp", addr)
+	if err != nil {
+		log.Fatalf("failed to listen: %v", err)
+	}
+
+	middlewareServer := grpc.NewServer(
+		grpc.StreamInterceptor(grpc_middleware.ChainStreamServer(grpc_prometheus.StreamServerInterceptor)),
+		grpc.UnaryInterceptor(grpc_middleware.ChainUnaryServer(grpc_prometheus.UnaryServerInterceptor)),
+	)
+
+	pb.RegisterPingerServer(middlewareServer, &server{})
+
+	grpc_prometheus.Register(middlewareServer)
+	grpc_prometheus.EnableHandlingTimeHistogram()
+
+	// Register reflection service on gRPC server.
+	reflection.Register(middlewareServer)
+
+	go func() {
+		errChan <- middlewareServer.Serve(lis)
+	}()
+
+}
+
+func serveHTTP(addr string, errChan chan error) {
+	http.Handle("/metrics", promhttp.Handler())
+	go func() {
+		errChan <- http.ListenAndServe(addr, nil)
+	}()
+}
+
+func serveAll(c *config) {
+	errChan := make(chan error, 10)
+
+	go serveHTTP(c.httpAddr, errChan)
+	go serveGRPC(c.grpcAddr, errChan)
+
+	signalChan := make(chan os.Signal, 1)
+	signal.Notify(signalChan, syscall.SIGINT, syscall.SIGTERM)
+	for {
+		select {
+		case err := <-errChan:
+			if err != nil {
+				fmt.Printf("%s \n", err)
+				os.Exit(1)
+			}
+		case <-signalChan:
+			fmt.Println("Shutdown signal received, exiting...")
+			os.Exit(0)
+		}
+	}
+}
+
+//
+// End Server
+//
+
+//
+// Client
+//
+
+//
+// End Client
+//
+
 func main() {
-	newCmd()
+	serverConf := newServerCmd()
+	serveAll(serverConf)
+
 }
